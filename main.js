6f5cdc2f8cd11e5a3e5a0a592e6d546494e1ff02
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-10-28 18:04:37 +0530

    array-print-twodigit-dowhile

---
+++ main.js
@@ -0,0 +1,17 @@
+//Separate two digit numbers of given array using do-while
+
+document.write("Separate two digit numbers of given array using do-while"+"<br>");
+document.write("-----------------------------------------"+"<br><br>");
+
+var a=[7,12,9,10,93,45,234,37,6,100];
+document.write("The array elements are:"+a+"<br>");
+document.write("The count of array elements :"+a.length+"<br><br>")
+document.write("The two digit array elements are:"+"<br>");
+let i=0;
+do{
+    if(a[i]>9 && a[i]<100){
+        document.write(a[i]+"<br>");
+    }
+    i++;
+}
+while(i<a.length)
